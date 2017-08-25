fb5c25b361ed04ba0b4b8b949f574510a304833e
Joshua Nance <joshua.h.nance@gmail.com>
2017-08-25 12:05:52 -0500

    Set up basic page routing

---
+++ src/server/pageHandler.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+exports.rootHandler = (req, res) => {
+  if (req.session.user) exports.index(req, res);
+  else res.send('Not logged in');
+}
+
+exports.index = (req, res) => {
+  res.send('Map index');
+}
+
+exports.canvas = (req, res) => {
+  res.send(`Map canvas for ${req.params.id}`);
+}
+
+exports.error = (req, res) => {
+  res.sendFile(path.join(__dirname, '../assets/biblethump.jpg'));
+}
